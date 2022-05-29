55e68177b8e26c4317def242dbc05972bb4cdd7b
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-29 00:08:15 -0500

    Add: Configuration of tokens

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -2,6 +2,13 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import MyTokenObtainPairSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
