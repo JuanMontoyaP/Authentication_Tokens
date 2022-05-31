5b993da046a47ec7fb491345348e9b242b34046f
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-30 23:18:23 -0500

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage'
+import Header  from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<HomePage />} exact/>
           <Route path="/login" element={<LoginPage />}/>
