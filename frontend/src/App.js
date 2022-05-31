5683bdbe5b7ae13c201db64deadb3dfafbbc32c7
Juan Pablo M <jpmontoya19@gmail.com>
2022-05-30 23:06:38 -0500

    Add The main and loging page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <div className="App">
-      <p>HOME</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} exact/>
+          <Route path="/login" element={<LoginPage />}/>
+        </Routes>
+      </Router>
     </div>
+
   );
 }
 
